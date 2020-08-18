3845dad9eb4d5138478529643069386394426b0d
Lewandy Dilone <lewandydilone1@live.com>
2020-08-18 01:15:51 -0400

    Improve example app

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <div class="container mx-auto mt-2">
-      <div class="font-bold text-lg mb-6">Vue permission</div>
+    <div class="container mx-auto ml-3 mt-2">
+      <div class="font-bold text-lg ml-4 mb-6">Vue permission</div>
       <div class="max-w-sm rounded overflow-hidden shadow-lg">
         <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">Amazing actions</div>
-          <p
-            class="text-gray-700 text-base"
-          >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+          <div class="font-bold text-xl mb-2">
+            Shows or hides element depending of user the permission
+          </div>
         </div>
         <div class="px-6 py-4">
-          <permission-provider :full="true" :permissionId="permissions.action1">
+          <permission-provider :permissionId="permissions.action1">
             <template v-slot>
               <button
                 class="mb-2 inline bg-gray-200 shadow-md rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-              >#action1</button>
-              <button
-                class="mb-2 inline bg-gray-200 shadow-md rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-              >#action1</button>
+              >
+                {{ "Action with permission " + permissions.action1 }}
+              </button>
             </template>
           </permission-provider>
           <permission-provider :permissionId="permissions.action2">
@@ -25,7 +23,15 @@
               <button
                 v-if="isAuthorized"
                 class="mb-2 inline bg-gray-200 shadow-md rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-              >#action2</button>
+              >
+                {{ "Action with permission " + permissions.action2 }}
+              </button>
+              <button
+                v-else
+                class="mb-2 inline bg-gray-200 shadow-md rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+              >
+                {{ "You can't see this. permissionId :" + permissions.action2 }}
+              </button>
             </template>
           </permission-provider>
           <permission-provider :permissionId="permissions.action3">
@@ -33,15 +39,9 @@
               <button
                 v-if="isAuthorized"
                 class="mb-2 inline bg-gray-200 shadow-md rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
-              >#action3</button>
-              <button
-                v-if="isAuthorized"
-                class="mb-2 inline bg-gray-200 shadow-md rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
-              >#action3</button>
-              <button
-                v-if="isAuthorized"
-                class="mb-2 inline bg-gray-200 shadow-md rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
-              >#action3</button>
+              >
+                {{ "Action with permission " + permissions.action3 }}
+              </button>
             </template>
           </permission-provider>
         </div>
@@ -68,5 +68,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
