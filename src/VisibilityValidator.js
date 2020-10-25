ab35e71e52dfe8bbeba3c267562e216e54d5eea6
Lewandy Dilone Bonifacio <lewandydilone1@live.com>
2020-10-24 23:37:03 -0400

    Refactor the plugin funtionality

--- src/VisibilityValidator.js
+++ src/VisibilityValidator.js
@@ -1,8 +1,7 @@
 export default {
-  name: "VueVisibility",
+  name: "VueVisibilityProvider",
   props: {
     identifier: {
-      type: Number,
       required: true,
     },
     disable: {
@@ -11,61 +10,25 @@ export default {
       default: false,
     },
   },
-  mounted() {
-    this.isAuthorized = this.verifyPermission(this.identifier);
-  },
-  render(createElement) {
-    const isAuthorized = this.verifyPermission(this.identifier);
-    const slots = this.$scopedSlots.default({
-      isAuthorized: isAuthorized,
-    });
+  render(context) {
+    const { identifier, disable } = context;
+    const permissions = context.$root.$vpermissions;
 
-    //Verify that has at least one node
-    if (!slots) {
-      return this.getDivWidthoutStyles(createElement, []);
+    if (!permissions) {
+      console.error("Permissions not found");
+      return;
     }
 
-    if (this.full) {
-      //Verify if full prop is passed to evaluate all child nodes.
-      return isAuthorized
-        ? slots.length === 1
-          ? slots
-          : this.getDivWidthoutStyles(createElement, slots)
-        : this.getDivWidthoutStyles(createElement, []);
+    const userCanSeeElement = permissions.find((item) => item === identifier);
+
+    if (!userCanSeeElement) {
+      if (disable) {
+        //TODO: Add disable styles and set not clickeble
+      }
+
+      return context.$slots.placeholder ? context.$slots.placeholder() : null;
     }
 
-    //Verify if the provider has only one node.
-    if (slots.length == 1) {
-      return isAuthorized
-        ? slots
-        : this.getDivWidthoutStyles(createElement, []);
-    }
-
-    //Create a container element for render multiple childrens.
-    return this.getDivWidthoutStyles(createElement, slots);
-  },
-  methods: {
-    getDivWidthoutStyles(createElement, vnodes) {
-      return createElement(
-        "div",
-        {
-          attrs: {
-            style: "all:unset",
-          },
-        },
-        vnodes
-      );
-    },
-    /**
-     * Verify if identifier is stored in the app instance.
-     * @param {String} permissionIdentifier
-     */
-    verifyPermission(permissionIdentifier) {
-      const criteria = (id) => {
-        return id === permissionIdentifier;
-      };
-
-      return this.$root.$vpermissions.find(criteria);
-    },
+    return this.$slots.default();
   },
 };
