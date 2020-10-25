096056d2c6ad967cf3083be2c6a623450a283db9
Lewandy Dilone Bonifacio <lewandydilone1@live.com>
2020-10-25 12:41:45 -0400

    Remove unused folder

--- example/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import VueVisibility from "../../src/index";
-
-const application = createApp(App);
-
-application.use(VueVisibility);
-
-application.mount("#app");
